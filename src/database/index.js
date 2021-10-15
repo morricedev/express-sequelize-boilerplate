450f87d856f91cdd147082cc11c47baa96596122
Maurício Sousa <morricedev@gmail.com>
2021-10-15 10:11:33 -0300

    Init models

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,12 @@
 const Sequelize = require("sequelize");
 const databaseConfig = require("../config/database");
 
+const Home = require("../models/Home");
+
+const models = [Home];
+
 const connection = new Sequelize(databaseConfig);
+
+models.forEach((model) => model.init(connection));
 
 module.exports = connection;
